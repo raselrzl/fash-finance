ad47170edad685b986d650ad77414addc9b52275
Rasel Miah <rasel6041@gmail.com>
2024-08-04 22:00:30 +0100

    Account balance and animated counter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
-type Props = {}
 
-const Home = (props: Props) => {
+
+const Home = () => {
+  const loggedIn={firstName:'Zaber'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and Transections."
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.56}
+          />
+        </header>
+      </div>
+    </section>
   )
 };
 
